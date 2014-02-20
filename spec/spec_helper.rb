31227d0d7b26a7bfe85fb3335202db8fa364f33b
Marcelo Goncalves <marcelocg@gmail.com>
2014-02-19 22:53:21 -0300

    Preparing to test with shared examples, as recommended by http://bit.ly/MdCufR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
-require_relative "../lib/cloud_capacitor/configuration"
-require_relative "../lib/cloud_capacitor/cloud_capacitor"
-require_relative "../lib/cloud_capacitor/err/invalid_config_name_error"
-require_relative "../lib/cloud_capacitor/err/invalid_configuration_file_error"
+Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
+Dir["./lib/cloud_capacitor/**/*.rb"].sort.each {|f| require f}
 
 require "yaml/store"
