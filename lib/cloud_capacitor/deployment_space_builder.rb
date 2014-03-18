ab9824e75c86f4c7cf6a26e6c10972feea3a9c40
Matheus Cunha <mathcunha@gmail.com>
2014-03-18 14:31:24 +0000

    building graph by price, mem or ecu

--- lib/cloud_capacitor/deployment_space_builder.rb
+++ lib/cloud_capacitor/deployment_space_builder.rb
@@ -4,24 +4,37 @@ module CloudCapacitor
   class DeploymentSpaceBuilder
 
     def self.graph_by_price(configurations:, max_price:, max_num_instances:4)
-      configs = configurations.sort { |x,y| x.price <=> y.price }
+      graph_by_prop(configurations, max_price, max_num_instances, "price")
+    end
+
+    def self.graph_by_mem(configurations:, max_price:, max_num_instances:4)
+      graph_by_prop(configurations, max_price, max_num_instances, "mem")
+    end
+
+    def self.graph_by_ecu(configurations:, max_price:, max_num_instances:4)
+      graph_by_prop(configurations, max_price, max_num_instances, "ecu")
+    end
+
+    private
+    def self.graph_by_prop(configurations, max_price, max_num_instances, prop_method)
+      configs = configurations.sort { |x,y| x.method(prop_method).call() <=> y.method(prop_method).call() }
       dg = Plexus::Digraph.new
-     
+
       config_groups = array_by_price(configurations, max_price, max_num_instances)
-      config_groups = config_groups.sort {|x,y| x.price <=> y.price}
+      config_groups = config_groups.sort {|x,y| x.method(prop_method).call() <=> y.method(prop_method).call()}
       #config_groups.each {|config_group| puts"#{config_group}"}
 
-      price = config_groups[0].price
+      prop = config_groups[0].method(prop_method).call()
       vertexes = []
       vertexes_old = nil
 
       i = 0
       until i >= config_groups.size() do
-        if(equal(price, config_groups[i].price, 0.01))
+        if(equal(prop, config_groups[i].method(prop_method).call(), 0.01))
           vertexes << config_groups[i]
         else
-          add_edges(vertexes_old, vertexes, dg)
-          price = config_groups[i].price
+          add_edges(vertexes_old, vertexes, dg, prop_method)
+          prop = config_groups[i].method(prop_method).call()
           vertexes_old = Array.new(vertexes)
           vertexes = []
           i = i - 1
@@ -29,12 +42,12 @@ module CloudCapacitor
         i += 1
       end
 
-      add_edges(vertexes_old, vertexes, dg)
+      add_edges(vertexes_old, vertexes, dg, prop_method)
 
       graph = Plexus::DirectedPseudoGraph.new(dg)
 
       #puts"graph vertices - #{graph.vertices.size}, graph edges - #{graph.edges.size}"
- 
+
       graph
     end
 
@@ -47,25 +60,6 @@ module CloudCapacitor
       end
     end
 
-
-    def self.new_edge(source, target, label)
-      #puts "source #{source} - target #{target} - label #{label.round(2)}"
-      Plexus::Arc.new(source, target, label.round(2))
-    end
-
-    def self.add_edges(vertexes_old, vertexes, arcs)
-      if(!vertexes_old.nil? && !vertexes.nil?)
-        vertexes_old.each do |vertex_old|
-          vertexes.each do |vertex|
-            arcs << new_edge(vertex_old, vertex, vertex.price-vertex_old.price)
-            arcs << new_edge(vertex, vertex_old, vertex_old.price-vertex.price)
-            #arcs << new_edge(vertex, vertex_old, vertex.price-vertex_old.price)
-          end
-        end
-      end
-      #puts ""
-    end
-    
     def self.array_by_price(configs, max_price, max_num_instances)
       config_groups = []
 
@@ -83,6 +77,23 @@ module CloudCapacitor
       end
 
       config_groups
+    end
+    
+    def self.new_edge(source, target, label)
+      #puts "source #{source} - target #{target} - label #{label.round(2)}"
+      Plexus::Arc.new(source, target, label.round(2))
+    end
+
+    def self.add_edges(vertexes_old, vertexes, arcs, prop_method)
+      if(!vertexes_old.nil? && !vertexes.nil?)
+        vertexes_old.each do |vertex_old|
+          vertexes.each do |vertex|
+            arcs << new_edge(vertex_old, vertex, vertex.method(prop_method).call() - vertex_old.method(prop_method).call())
+            #arcs << new_edge(vertex, vertex_old, vertex_old.method(prop_method).call() - vertex.method(prop_method).call())
+          end
+        end
+      end
+      #puts ""
     end
   end
 end
