b4ea85f5d47167a1df953ecebd5b615f44cc834f
Matheus Cunha <mathcunha@gmail.com>
2014-03-18 03:29:03 +0000

    return the graph by price

--- lib/cloud_capacitor/deployment_space_builder.rb
+++ lib/cloud_capacitor/deployment_space_builder.rb
@@ -3,9 +3,9 @@ module CloudCapacitor
 
   class DeploymentSpaceBuilder
 
-    def self.graph_by_price(configurations:, max_price:, max_num_instances:2)
+    def self.graph_by_price(configurations:, max_price:, max_num_instances:4)
       configs = configurations.sort { |x,y| x.price <=> y.price }
-      graph = Plexus::DirectedPseudoGraph.new
+      arcs = []
      
       config_groups = array_by_price(configurations, max_price, max_num_instances)
       config_groups = config_groups.sort {|x,y| x.price <=> y.price}
@@ -14,33 +14,56 @@ module CloudCapacitor
       price = config_groups[0].price
       vertexes = []
       vertexes_old = nil
-      for i in 0..config_groups.size()-1
-        if(price == config_groups[i].price)
+
+      i = 0
+      until i >= config_groups.size() do
+        if(equal(price, config_groups[i].price, 0.01))
           vertexes << config_groups[i]
         else
-          add_edges(vertexes_old, vertexes, graph)
+          add_edges(vertexes_old, vertexes, arcs)
           price = config_groups[i].price
-          vertexes_old = vertexes
+          vertexes_old = Array.new(vertexes)
           vertexes = []
           i = i - 1
         end
+        i += 1
       end
 
-      add_edges(vertexes_old, vertexes, graph)
+      add_edges(vertexes_old, vertexes, arcs)
 
-      puts"#{graph.edges.sort}"
+
+      graph = Plexus::DirectedPseudoGraph.new(arcs)
+
+      puts"graph - #{graph.edges}"
+      
       graph
     end
 
-    def self.add_edges(vertexes_old, vertexes, graph)
+    def self.equal(prop, prop2, error)
+      diff = prop2 - prop
+      if (diff < error)
+        return true
+      else
+        return false
+      end
+    end
+
+
+    def self.new_edge(source, target, label)
+      puts "source #{source} - target #{target} - label #{label}"
+      Plexus::Arc.new(source, target, label)
+    end
+
+    def self.add_edges(vertexes_old, vertexes, arcs)
       if(!vertexes_old.nil? && !vertexes.nil?)
         vertexes_old.each do |vertex_old|
           vertexes.each do |vertex|
-            graph.add_edge(vertex_old, vertex, vertex.price-vertex_old.price)
-            graph.add_edge(vertex, vertex_old, vertex_old.price-vertex.price)
+            arcs << new_edge(vertex_old, vertex, vertex.price-vertex_old.price)
+            arcs << new_edge(vertex, vertex_old, vertex_old.price-vertex.price)
           end
         end
       end
+      puts ""
     end
     
     def self.array_by_price(configs, max_price, max_num_instances)
