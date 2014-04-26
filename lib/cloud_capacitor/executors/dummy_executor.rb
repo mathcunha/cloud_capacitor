9a3a2d615f2a8922182bc073c256ec424dfc4f4c
Marcelo Goncalves <marcelocg@gmail.com>
2014-04-25 22:19:01 -0300

    Fixed a shameful error. Remember to test always!

--- lib/cloud_capacitor/executors/dummy_executor.rb
+++ lib/cloud_capacitor/executors/dummy_executor.rb
@@ -8,7 +8,7 @@ module CloudCapacitor
         log.debug "DummyExecutor: executing performance test..."
         sleep 0.1
         sla = Settings.capacitor.sla
-        fake_response_time = Rand.rand (sla * 0.9)..(sla * 1.2)
+        fake_response_time = Random.rand (sla * 0.9)..(sla * 1.2)
         log.debug "DummyExecutor: fake response time = #{fake_response_time}ms for a #{sla}ms SLA"
         Result.new(value: fake_response_time,cpu: 75.5, mem: 78.9)
       end
