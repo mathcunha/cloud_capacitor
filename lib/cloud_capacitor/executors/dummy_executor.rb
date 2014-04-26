e5c8286483cb5a6613c7bd40028755593c2dc4aa
Marcelo Goncalves <marcelocg@gmail.com>
2014-04-25 22:02:21 -0300

    Random fake results for tests purposes

--- lib/cloud_capacitor/executors/dummy_executor.rb
+++ lib/cloud_capacitor/executors/dummy_executor.rb
@@ -5,9 +5,12 @@ module CloudCapacitor
       include Log
 
       def run(configuration:, workload:)
-        log.debug "DummyExecutor: returning result"
+        log.debug "DummyExecutor: executing performance test..."
         sleep 0.1
-        Result.new(value: 2100, cpu: 75.5, mem: 78.9)
+        sla = Settings.capacitor.sla
+        fake_response_time = Rand.rand (sla * 0.9)..(sla * 1.2)
+        log.debug "DummyExecutor: fake response time = #{fake_response_time}ms for a #{sla}ms SLA"
+        Result.new(value: fake_response_time,cpu: 75.5, mem: 78.9)
       end
     end
 
