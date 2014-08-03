e7ebecea45ff7e98106ad5aea81dcc66c4874a4b
Marcelo Goncalves <marcelocg@gmail.com>
2014-08-03 10:55:59 -0300

    New strategy plus improvements

---
+++ lib/cloud_capacitor/strategies/mcg_strategy.rb
@@ -0,0 +1,44 @@
+module CloudCapacitor
+  module Strategies
+    class MCG_Strategy < Strategy
+      attr_accessor :attitude
+
+    	def initialize
+    		super
+    		@wkl_attitude = :optimistic
+        @cfg_attitude = :optimistic
+    	end
+
+    	def attitude(workload:, config:)
+        @wkl_attitude = workload if [:optimistic, :conservative, :pessimistic].include? workload
+    		@cfg_attitude = config if [:optimistic, :conservative, :pessimistic].include? config
+    	end
+
+      def select_initial_workload(workload_list)
+        # log.debug "Strategy: Selecting initial workload with #{@wkl_attitude} attitude"
+        case @wkl_attitude
+        when :pessimistic
+          workload_list.first
+        when :optimistic
+          workload_list.last
+        when :conservative
+          workload_list[workload_list.size / 2]
+        end
+      end
+
+      def select_initial_configuration
+        # log.debug "Strategy: Selecting initial configuration with #{@cfg_attitude} attitude"
+        case @cfg_attitude
+        when :pessimistic
+          capacitor.deployment_space.last
+        when :optimistic
+          capacitor.deployment_space.first
+        when :conservative
+          capacitor.deployment_space.mean
+        end
+        # log.debug "Strategy: Initial configuration set to #{capacitor.deployment_space.current_config}"
+      end
+
+    end
+  end
+end
--- lib/cloud_capacitor/strategies/strategy.rb
+++ lib/cloud_capacitor/strategies/strategy.rb
@@ -10,25 +10,25 @@ module CloudCapacitor
       end
 
       def select_initial_workload(workload_list)
-        log.debug "Strategy: Initial workload set to #{workload_list[0]}"
+        # log.debug "Strategy: Initial workload set to #{workload_list[0]}"
         workload_list[0]
       end
       
       def raise_workload
         unexplored = capacitor.unexplored_workloads.reject { |w| w <= capacitor.current_workload }.sort!
-        log.debug "Strategy: raising workload from #{capacitor.current_workload} to #{unexplored.first}"
+        # log.debug "Strategy: raising workload from #{capacitor.current_workload} to #{unexplored.first}"
         unexplored.first
       end
 
       def lower_workload
         unexplored = capacitor.unexplored_workloads.reject { |w| w >= capacitor.current_workload }.sort!
-        log.debug "Strategy: lowering workload from #{capacitor.current_workload} to #{unexplored.last}"
+        # log.debug "Strategy: lowering workload from #{capacitor.current_workload} to #{unexplored.last}"
         unexplored.last
       end
       
       def select_initial_configuration
         capacitor.deployment_space.first
-        log.debug "Strategy: Initial configuration set to #{capacitor.deployment_space.current_config}"
+        # log.debug "Strategy: Initial configuration set to #{capacitor.deployment_space.current_config}"
       end
       
       def select_lower_configurations_based_on(result)
