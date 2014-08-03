ebff741d9980dd0c2ce0240c0b49dcda1bd95605
Marcelo Goncalves <marcelocg@gmail.com>
2014-08-03 14:09:42 -0300

    Logger simplification

--- lib/cloud_capacitor/util/logger.rb
+++ lib/cloud_capacitor/util/logger.rb
@@ -2,13 +2,7 @@ require "logger"
 
 module Log
 
-  def included(mod)
-    @log_output ||= CloudCapacitor::Settings.logger.log_to_file == 0 ? 
-        STDOUT : File.open(File.join( File.expand_path('../..', __FILE__), "cloud_capacitor.log"), "a")
-  end
-
   def log
-    # @logger ||= defined?(Rails) ? Rails.logger : Logger.new(@log_output)
     @logger ||= defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
     @logger.formatter = proc do |severity, datetime, progname, msg|
       "- #{msg}\n"
